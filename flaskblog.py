d1c1861ead36ca59987368ca60610920f04ccab5
szponovic <tomasz.kamil.biskupski@gmail.com>
2019-10-11 21:25:17 +0200

    home, about pages and css

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Tomasz Biskupski',
+        'title': 'Blog Post One',
+        'content': 'First post content',
+        'date_posted': '01 october 2019'
+    },
+{
+        'author': 'Lord Szponovic',
+        'title': 'Blog Post Two',
+        'content': 'Second post content',
+        'date_posted': '02 october 2019'
+    }
+]
+
 @app.route("/")
-def hello():
-    return "<h1>HOME PAGE</h1>"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>ABOUT PAGE</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
